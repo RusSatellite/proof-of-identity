e4f70d8ffe2b5f1223682515ac77942461665b97
unknown <russatellite23@gmail.com>
2022-03-16 00:34:44 -0700

    v0

--- src/App.js
+++ src/App.js
@@ -1,77 +1,94 @@
-import { useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import React from "react";
 
-function App() {
-  const [show, setShow] = useState(false);
+import { ChakraProvider, Flex, Button, Text } from "@chakra-ui/react";
+import PaypalExpressBtn from "react-paypal-express-checkout";
 
-    const handlePaypal = async(e) => {
-        e.preventDefault();
-        setShow(!show);
-        console.log(show);
-    }
 
-    const handleCoinbase = async(e) => {
-        e.preventDefault();
-        const price = { amount: '500' };
-        axios.post('https://humanincome.org/api/mainnet/coinbase/pay', price)
-        .then(res => {
-            if (res.data.success) {
-                window.open(res.data.data.hosted_url, "_parent");
-            }
-        });
-    }
+export default function App() {
+  const onSuccess = (payment) => {
+    // Congratulations, the payment was successful!
+    console.log("The payment was successful!", payment);
+  };
 
-    return (
-        <div>
-            <div className="app-content-height d-flex flex-column align-items-center justify-content-center payment-modal-main">
-                <div className="fw-600 fs-2p0 cod-gray">You will pay 500€ with</div>
-                <div>
-                    <button className='btn btn-app-dark-gray py-2 position-relative'  style={{minHeight: "3rem", minWidth: "6rem", margin: "10px"}} onClick={handlePaypal}>Paypal</button>
-                    <button className='btn btn-app-dark-gray py-2 position-relative'  style={{minHeight: "3rem", minWidth: "6rem", margin: "10px"}} onClick={handleCoinbase}>Coinbase</button>
-                </div>
-                {
-                    show && 
-                    <PayPalScriptProvider
-                        options={{
-                            "client-id": "AV1hGBARJo3cQMMhODOuVhH1XMQSRlUCtE3ogDRfGskShI39eu1aJbjs63xd8U4PXH3dpLYBtBqluHf7",
-                        }}
-                    >
-                        <PayPalButtons
-                            fundingSource='paypal'
-                            createOrder={(data, actions) => {
-                                return actions.order
-                                .create({
-                                    purchase_units: [
-                                        {
-                                            amount: {
-                                                currency_code: "USD",
-                                                value: "500",
-                                            },
-                                        },
-                                    ],
-                                })
-                                .then((orderId) => {
-                                    // Your code here after create the order
-                                    console.log('data', data);
-                                    console.log('orderID', orderId);
-                                    return orderId;
-                                });
-                            }}
-                            onApprove={function (data, actions) {
-                                return actions.order
-                                .capture()
-                                .then(function () {
-                                    console.log('Approve Data', data);
-                                });
-                            }}
-                        />
-                    </PayPalScriptProvider>
-                }
-            </div>
-        </div>
-    );
+  const onCancel = (data) => {
+    // User pressed 'cancel' or closed the PayPal popup window
+    console.log("The payment was cancelled!", data);
+  };
+
+  const onError = (err) => {
+    // The main PayPal 's script can't be loaded
+    console.log("Error!", err);
+  };
+
+  const onCoinbase = (e) => {
+    e.preventDefault();
+
+  }
+
+  // Using sandbox for testing only
+  let env = "sandbox";
+  // Let's set our currency here
+  let currency = "USD";
+  // Testing total amount
+  let total = 10;
+
+  const client = {
+    sandbox: "ASKubZJSMqA7_0E2HE3yRKIej3xO92wyK6j6PKSfyyDZljGO23sh_xnOyHH7wIYHR2nPSLZjTZiihw6B"
+  };
+
+  return (
+    <ChakraProvider>
+      <Flex
+        margin={4}
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Flex
+          padding={4}
+          width="300px"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          borderWidth="2px"
+          borderRadius="lg"
+        >
+          <Text fontWeight="bold" margin={3}>500 EUR with Paypal</Text>
+          <PaypalExpressBtn
+            env={env}
+            client={client}
+            currency={currency}
+            total={total}
+            onError={onError}
+            onSuccess={onSuccess}
+            onCancel={onCancel}
+            style={{ shape: "rect", size: "medium", margin: "1.5rem" }}
+          />
+        </Flex>
+        <Flex
+          padding={4}
+          marginTop="27px"
+          width="300px"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          borderWidth="2px"
+          borderRadius="lg"
+        >
+          <Text fontWeight="bold" margin={3}>500 EUR with Coinbase</Text>
+          <Button
+            style={{
+              backgroundColor: "#ffc439",
+              width: "95%",
+              color: "black",
+              borderRadius: "3px",
+              height: "35px"
+            }}
+          >
+            Coinbase Checkout
+          </Button>
+        </Flex>
+      </Flex>
+    </ChakraProvider>
+  );
 }
-
-export default App;
